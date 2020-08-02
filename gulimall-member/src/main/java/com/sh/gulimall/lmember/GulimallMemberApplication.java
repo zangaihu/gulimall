64d06b13b34b6fd4d8fc71351fead3c8596e8dbc
Sun <1125245923@qq.com>
2020-08-02 12:55:47 +0800

    项目初始化

---
+++ gulimall-member/src/main/java/com/sh/gulimall/lmember/GulimallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.sh.gulimall.lmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallMemberApplication.class, args);
+    }
+
+}
